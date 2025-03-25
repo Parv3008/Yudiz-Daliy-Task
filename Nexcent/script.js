54ccb08c2b0a1a6aa67004cc04653e20996a3bde
Parv3008 <140480890+Parv3008@users.noreply.github.com>
2025-03-25 19:01:52 +0530

    Add files via upload

--- Nexcent/script.js
+++ Nexcent/script.js
@@ -22,21 +22,21 @@ const cardData = [
   },
 ];
 
-// hero card create
+
 const heroContainer = document.querySelector(".hero-container");
 
 cardData.map((card) => {
   const cardElement = document.createElement("div");
   const cardContent = `
-        <div class="row d-flex align-content-center justify-content-between ps ">
-                  <div class=" col-md-12 col-lg-7 left">
-                      <h1 class="fw-bold w-100 fw-semibold">${card.title}<span class="">${card.spantitle}</span></h1>
+        <div class="row slider">
+                  <div class="col-md-12 col-lg-7 left">
+                      <h1 class="fw-bold w-100 fw-semibold">${card.title}<span>${card.spantitle}</span></h1>
                       <p class="pb-2 pt-2 fw-normal">${card.description}</p>
                       <button class="mybtn register">Register</button>
 
                   </div>
-                  <div class=" col-md-12 col-lg-5 pt-xs-0 ill-img placeholder-glow">
-                      <img src="${card.imageUrl}" alt="" class="ill-img " id="my-Image">
+                  <div class=" col-md-12 col-lg-5 pt-xs-0">
+                      <img src="${card.imageUrl}" alt="" class="img-fluid" id="my-Image">
                   </div>
               </div>
       `;
